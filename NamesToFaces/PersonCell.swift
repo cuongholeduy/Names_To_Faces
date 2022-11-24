bc7b4cc96cbf6c459e47f0c6a87b3d54a5469445
CuongHo <cuong.holeduy@gmail.com>
2022-11-24 22:11:16 +0700

    done

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Duy Cuong on 24/11/2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
